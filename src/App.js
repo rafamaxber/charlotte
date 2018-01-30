5ad477815397dbc0342592fdc961730386c77f41
Rafael Bernardo <rafamaxber@gmail.com>
2018-01-30 00:05:16 -0200

    clean app

--- src/App.js
+++ src/App.js
@@ -9,27 +9,11 @@ const Home = LoadableWrapper({
   loader: () => import(/*webpackChunkName: "Home"*/'./containers/Home/Home.js'),
 });
 
-const About = LoadableWrapper({
-  loader: () => import(/*webpackChunkName: "About"*/'./containers/About/About.js'),
-});
-
-const Blog = LoadableWrapper({
-  loader: () => import(/*webpackChunkName: "Blog"*/'./containers/Blog/Blog.js'),
-});
-
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/blog">Blog</Link>
-          <hr />
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/blog" component={Blog} />
-        </div>
+        <Route path="/" exact component={Home} />
       </Router>
     );
   }
