0b206aac94208c00b16549415e9123d0e4a00f6c
Rafael Bernardo <rafamaxber@gmail.com>
2018-02-04 03:29:52 -0200

    start link all components

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,9 +4,18 @@ import HeaderNavigation from '../../components/HeaderNavigation/HeaderNavigation
 import Hero from '../../components/Hero/Hero';
 import BoxCalendar from '../../components/BoxCalendar/BoxCalendar';
 import BoxTitle from '../../components/BoxTitle/BoxTitle';
+import FilterRange from '../../components/FilterRange/FilterRange';
+import FilterRate from '../../components/FilterRate/FilterRate';
+import Card from '../../components/Card/Card';
 
 const WrapperListHotels = ({ children }) => (
   <div className="wrapperListHotels">
+    {children}
+  </div>
+);
+
+const WrapperFilters = ({ children }) => (
+  <div className="wrapperFilters">
     {children}
   </div>
 );
@@ -21,6 +30,11 @@ const HomePage = () => (
       <BoxTitle>
         Best choices between __date_start__ and __date_end__
       </BoxTitle>
+      <WrapperFilters>
+        <FilterRange></FilterRange>
+        <FilterRate></FilterRate>
+      </WrapperFilters>
+      <Card></Card>
     </WrapperListHotels>
 
   </Layout>
